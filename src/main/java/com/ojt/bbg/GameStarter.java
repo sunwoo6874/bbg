e060db048217d1f48aaea712cd7f68cd250c0492
sunwoo_baek <sunwoo.baek@llsollu.com>
2024-09-23 15:41:58 +0900

    minor change

--- src/main/java/com/ojt/bbg/GameStarter.java
+++ src/main/java/com/ojt/bbg/GameStarter.java
@@ -7,12 +7,11 @@ public class GameStarter {
     private final static Logger logger = LoggerFactory.getLogger("GameStarter");
 
     public static void main(String[] args) {
-        if (args[0].equals("wiki")) {
-            logger.debug("Initiating NamuWiki Numeric Baseball Game.");
-            new com.ojt.bbg.namuWiki.BaseBallGame().Game(args[1]); // coin
+        logger.debug("Initiating " + args[0] + " Numeric Baseball Game.");
 
+        if (args[0].equals("wiki")) {
+            new com.ojt.bbg.namuWiki.BaseBallGame().Game(args[1]); // coin
         } else if (args[0].equals("book")) {
-            logger.debug("Initiating HelloCoding Numeric Baseball Game.");
             new com.ojt.bbg.helloCoding.BaseBallGame().Game(args[1]);
         }
     }
